ed94aa0893121e84b06aa9f64485061826f5df35
Misode <Misoloo64@gmail.com>
2021-02-13 15:01:01 +0100

    Use subprojects

--- plugins.py
+++ plugins.py
@@ -1,8 +1,7 @@
-from beet import Context, DataPack, Function
+from beet import Context, DataPack, Function, FunctionTag, subproject
 import os
 import json
-
-from beet.library.data_pack import FunctionTag
+import re
 
 load = DataPack("load", path="load")
 
@@ -20,53 +19,67 @@ def scan(ctx: Context):
 def build(ctx: Context):
     for m in ctx.meta["modules"]:
         module = ctx.meta["modules"][m]
-        pack = DataPack(m, path=module["path"])
-        pack.description = [
-            module["name"] + "\n",
-            { "text": "Gamemode 4 for 1.16", "color": "aqua" }
-        ]
-        pack.pack_format = ctx.meta["pack_format"]
+        ctx.require(subproject({
+            "pipeline": ["plugins.build_module"],
+            "data_pack": {
+                "load": [module["path"]],
+                "name": m,
+                "pack_format": ctx.meta["pack_format"]
+            },
+            "output": "out",
+            "meta": {
+                "modules": ctx.meta["modules"],
+                "module": module
+            }
+        }))
 
-        build_module(ctx, module, pack)
-        pack.save("out", overwrite=True)
 
-
-def build_module(ctx: Context, module, pack: DataPack):
+def build_module(ctx: Context):
+    module = ctx.meta["module"]
+    id = module["id"]
+    short_id = re.sub(r"^gm4_", "", id)
+    mainspace = ctx.data[id]
     dependencies = module.get("dependencies", dict())
-    if module["id"] != "gm4":
+
+    ctx.data.description = [
+        module["name"] + "\n",
+        { "text": "Gamemode 4 for 1.16", "color": "aqua" }
+    ]
+
+    if id != "gm4":
         dependencies["gm4"] = ctx.meta["modules"]["gm4"]["version"]
 
     if dependencies:
-        load_function = [f"execute {' '.join([f'if score {d} load matches {dependencies[d]}' for d in dependencies])} run scoreboard players set {module['id']} load {module['version']}"]
+        load_function = [f"execute {' '.join([f'if score {d} load matches {dependencies[d]}' for d in dependencies])} run scoreboard players set {id} load {module['version']}"]
     else:
-        load_function = [f"scoreboard players set {module['id']} load {module['version']}"]
+        load_function = [f"scoreboard players set {id} load {module['version']}"]
 
     for d in dependencies:
         load_function.append(f"execute unless score {d} load matches {dependencies[d]} run data modify storage gm4:load queue append value {{type:\"missing\",module:\"{module['name']}\",require:\"{ctx.meta['modules'][d]['name']}\"}}")
     load_function.append("")
-    load_function.append(f"execute if score {module['id']} load matches {module['version']} run function {module['id']}:init")
+    load_function.append(f"execute if score {id} load matches {module['version']} run function {id}:init")
 
     init_function = []
-    if f"{module['id']}:init" in pack.functions:
-        init_function.extend(pack.functions[f"{module['id']}:init"].lines)
+    if "init" in mainspace.functions:
+        init_function.extend(mainspace.functions["init"].lines)
 
-    init_function.append(f"execute unless score {module['id']} gm4_modules matches 1 run data modify storage gm4:log queue append value {{type:\"install\",module:\"{module['name']}\"}}")
-    init_function.append(f"scoreboard players set {module['id']} gm4_modules 1")
+    init_function.append(f"execute unless score {short_id} gm4_modules matches 1 run data modify storage gm4:log queue append value {{type:\"install\",module:\"{module['name']}\"}}")
+    init_function.append(f"scoreboard players set {short_id} gm4_modules 1")
     init_function.append("")
 
     for e in module.get("entrypoints", []):
-        load_function.append(f"execute unless score {module['id']} load matches {module['version']} run schedule clear {module['id']}:{e}")
-        init_function.append(f"schedule function {module['id']}:{e} 1t")
+        load_function.append(f"execute unless score {id} load matches {module['version']} run schedule clear {id}:{e}")
+        init_function.append(f"schedule function {id}:{e} 1t")
 
-    pack.functions[f"{module['id']}:load"] = Function(load_function)
-    pack.functions[f"{module['id']}:init"] = Function(init_function)
+    mainspace.functions["load"] = Function(load_function)
+    mainspace.functions["init"] = Function(init_function)
 
-    pack.merge(load)
+    ctx.data.merge(load)
 
-    pack.function_tags["load:load"] = FunctionTag({ "values": [f"#load:{module['id']}"] })
+    ctx.data.function_tags["load:load"] = FunctionTag({ "values": [f"#load:{id}"] })
     load_tag = [f"#load:{d}" for d in dependencies]
-    load_tag.append(f"{module['id']}:load")
-    pack.function_tags[f"load:{module['id']}"] = FunctionTag({ "values": load_tag })
+    load_tag.append(f"{id}:load")
+    ctx.data.function_tags[f"load:{id}"] = FunctionTag({ "values": load_tag })
 
     for d in dependencies:
-        pack.function_tags[f"load:{d}"] = FunctionTag({ "values": [] })
+        ctx.data.function_tags[f"load:{d}"] = FunctionTag({ "values": [] })
